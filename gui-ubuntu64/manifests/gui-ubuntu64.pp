386049ac524e4e7caa6a2196b648d0a3f9caead0
Halvard Skogsrud <hskogsru@thoughtworks.com>
2012-04-21 03:04:54 +1000

    Change Ubuntu keybindings

--- gui-ubuntu64/manifests/gui-ubuntu64.pp
+++ gui-ubuntu64/manifests/gui-ubuntu64.pp
@@ -4,6 +4,7 @@ include fix::lucid
 include fonts::microsoft
 include google::chrome
 include timezone::sydney
+include ubuntu::keybindings
 include utils::base
 include viewer::vlc
 include xwindows::hideerrors
