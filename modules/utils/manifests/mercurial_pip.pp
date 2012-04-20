b116d4addc734e1e885ab00db329d806ff429cae
Halvard Skogsrud <hskogsru@thoughtworks.com>
2012-04-21 03:08:35 +1000

    Installing Mercurial via PIP now works on both Ubuntu and CentOS

--- modules/utils/manifests/mercurial_pip.pp
+++ modules/utils/manifests/mercurial_pip.pp
@@ -1,36 +1,11 @@
 class utils::mercurial_pip {
-  include repos::epel
-
   package { 'python':
     ensure => present,
   }
 
-  package { ['python-devel', 'python-docutils']:
+  package { 'python-docutils':
     ensure => present,
     require => Package['python'],
-  }
-
-  package { 'python-pip':
-    ensure => present,
-    require => Package['epel-release', 'python'],
-  }
-
-  file { '/usr/bin/pip':
-    ensure => link,
-    target => '/usr/bin/pip-python',
-    require => Package['python-pip'],
-  }
-
-  package { 'mercurial':
-    provider => pip,
-    ensure => present,
-    require => [File['/usr/bin/pip'], Package['python-devel', 'python-docutils']],
-  }
-
-  package { 'keyring':
-    provider => pip,
-    ensure => present,
-    require => File['/usr/bin/pip'],
   }
 
   package { 'mercurial_keyring':
@@ -39,10 +14,67 @@ class utils::mercurial_pip {
     require => Package['keyring', 'mercurial'],
   }
 
-  file { '/home/vagrant/.hgrc':
-    ensure => present,
-    source => '/vagrant-share/apps/hgrc',
-    require => Package['mercurial_keyring'],
+  case $operatingsystem {
+    'CentOS': {
+      include yumrepos::epel
+
+      package { 'python-devel':
+        ensure => present,
+        require => Package['python'],
+      }
+
+      package { 'python-pip':
+        ensure => present,
+        require => Package['epel-release', 'python'],
+      }
+
+      file { '/usr/bin/pip':
+        ensure => link,
+        target => '/usr/bin/pip-python',
+        require => Package['python-pip'],
+      }
+
+      package { 'mercurial':
+        provider => pip,
+        ensure => present,
+        require => [File['/usr/bin/pip'], Package['python-devel', 'python-docutils']],
+      }
+
+      package { 'keyring':
+        provider => pip,
+        ensure => present,
+        require => File['/usr/bin/pip'],
+      }
+
+      file { '/home/vagrant/.hgrc':
+        ensure => present,
+        source => '/vagrant-share/apps/hgrc',
+        require => Package['mercurial_keyring'],
+      }
+    }
+    'Ubuntu': {
+      package { 'python-dev':
+        ensure => present,
+        require => Package['python'],
+      }
+
+      package { 'python-pip':
+        ensure => present,
+        require => Package['python'],
+      }
+
+      package { 'mercurial':
+        provider => pip,
+        ensure => present,
+        require => Package['python-dev', 'python-docutils', 'python-pip'],
+      }
+
+      package { 'keyring':
+        provider => pip,
+        ensure => present,
+        require => Package['python-pip'],
+      }
+    }
   }
 }
 
