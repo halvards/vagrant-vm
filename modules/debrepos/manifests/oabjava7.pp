82638ffaf2444f33fe8d9b302ad792a5c1d2360f
Halvard Skogsrud <halvard@skogsrud.com>
2013-03-27 11:54:18 +1100

    Create recipes for local apt repos for Ubuntu Java

---
+++ modules/debrepos/manifests/oabjava7.pp
@@ -0,0 +1,20 @@
+class debrepos::oabjava7 {
+  include utils::base
+
+  if ! defined(Wget::Fetch['oab-java']) {
+    wget::fetch { 'oab-java':
+      source      => 'https://raw.github.com/flexiondotorg/oab-java6/master/oab-java.sh',
+      destination => '/tmp/oab-java.sh',
+    }
+  }
+
+  exec { 'add-local-oab-java7-apt-repo':
+    command   => '/bin/bash /tmp/oab-java.sh -7',
+    cwd       => '/tmp',
+    creates   => '/var/local/oab/srcs/oracle-java7.git',
+    timeout   => 1800, # seconds
+    logoutput => true,
+    require   => Wget::Fetch['oab-java'],
+  }
+}
+
