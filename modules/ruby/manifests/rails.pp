37e2154a1a9e97db3ceb7d8604cb61deedf1c6dc
Halvard Skogsrud <halvard@skogsrud.com>
2013-04-03 16:38:28 +1100

    Updating Rails version in recipe

--- modules/ruby/manifests/rails.pp
+++ modules/ruby/manifests/rails.pp
@@ -3,15 +3,11 @@ class ruby::rails {
 
   package { 'rails':
     provider => gem,
-    ensure   => '3.2.11',
+    ensure   => '3.2.13',
     require  => Class['ruby::base'],
   }
 
   package { ['sqlite3', 'libsqlite3-dev', 'libpq-dev']:
-    ensure => present,
-  }
-
-  package { 'nodejs':
     ensure => present,
   }
 }
