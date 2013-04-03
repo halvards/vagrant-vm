3acb4c6d25b74c5b0150e42e336fc7cab3989f91
Halvard Skogsrud <halvard@skogsrud.com>
2013-04-03 18:52:54 +1100

    Simplifying Git recipe since Ubuntu has renamed the package

--- modules/utils/manifests/git.pp
+++ modules/utils/manifests/git.pp
@@ -1,17 +1,7 @@
 class utils::git {
   if ! defined(Package['git']) {
-    case $operatingsystem {
-      'CentOS': {
-        package { 'git':
-          ensure => present,
-        }
-      }
-      'Ubuntu': {
-        package { 'git':
-          ensure => present,
-          name => 'git-core',
-        }
-      }
+    package { 'git':
+      ensure => present,
     }
   }
 }
