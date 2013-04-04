03b23fa7eddc1e541c9b9b8fe0a6a6b403ae6867
Halvard Skogsrud <halvard@skogsrud.com>
2013-04-04 18:38:25 +1100

    Log some output when downloading files with wget

--- modules/wget/manifests/fetch.pp
+++ modules/wget/manifests/fetch.pp
@@ -1,18 +1,20 @@
 define wget::fetch($source,$destination) {
   if $http_proxy {
     exec { "wget-$name":
-      command     => "/usr/bin/wget --output-document=$destination $source",
+      command     => "/usr/bin/wget --no-verbose --output-document=$destination $source",
       creates     => $destination,
       timeout     => 3600, # seconds
+      logoutput   => true,
       require     => Package['wget'],
       environment => [ "HTTP_PROXY=$http_proxy", "http_proxy=$http_proxy" ],
     }
   } else {
     exec { "wget-$name":
-      command => "/usr/bin/wget --output-document=$destination $source",
-      creates => $destination,
-      timeout => 3600, # seconds
-      require => Package['wget'],
+      command   => "/usr/bin/wget --no-verbose --output-document=$destination $source",
+      creates   => $destination,
+      timeout   => 3600, # seconds
+      logoutput => true,
+      require   => Package['wget'],
     }
   }
 }
