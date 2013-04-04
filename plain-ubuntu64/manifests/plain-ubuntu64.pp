1ac242586513b341f24824764506499028261ed6
Halvard Skogsrud <halvard@skogsrud.com>
2013-04-04 18:37:56 +1100

    Typo

--- plain-ubuntu64/manifests/plain-ubuntu64.pp
+++ plain-ubuntu64/manifests/plain-ubuntu64.pp
@@ -1,5 +1,5 @@
 stage { 'first': before => Stage['main'], } # 'main' is the default single stage
-class { 'ubuntu::fixes': stage => 'first', } # Ensure basic fixes are install first
+class { 'ubuntu::fixes': stage => 'first', } # Ensure basic fixes are installed first
 
 include timezone::sydney
 include utils::base
