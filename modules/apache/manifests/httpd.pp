81447d5e60051c5352fcd2c3d04cef2e1a7f91c3
Halvard Skogsrud <halvard@skogsrud.com>
2013-04-05 16:26:38 +1100

    Fixing Apache httpd recipe for Ubuntu

--- modules/apache/manifests/httpd.pp
+++ modules/apache/manifests/httpd.pp
@@ -1,37 +1,69 @@
 class apache::httpd {
-  package { ['httpd', 'mod_ssl']:
-    ensure => present,
-  }
+  case $operatingsystem {
+    'CentOS': {
+      package { ['httpd', 'mod_ssl']:
+        ensure => present,
+      }
 
-  group { 'apache':
-    ensure  => present,
-    require => Package['httpd'],
-  }
+      group { 'apache':
+        ensure  => present,
+        require => Package['httpd'],
+      }
 
-  user { 'apache':
-    ensure => present,
-    require => Group['apache'],
-  }
+      user { 'apache':
+        ensure => present,
+        require => Group['apache'],
+      }
 
-  vagrant::group { 'vagrant-apache':
-    group => 'apache',
-    require => Group['apache'],
-  }
+      vagrant::group { 'vagrant-apache':
+        group => 'apache',
+        require => Group['apache'],
+      }
 
-  service { 'httpd':
-    enable => true,
-    ensure => running,
-    hasstatus => true,
-    hasrestart => true,
-    require => Package['httpd'],
-  }
+      service { 'httpd':
+        enable => true,
+        ensure => running,
+        hasstatus => true,
+        hasrestart => true,
+        require => Package['httpd'],
+      }
 
-  file { '/var/log/httpd':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => 0755,
-    require => Package['httpd'],
+      file { '/var/log/httpd':
+        ensure => directory,
+        owner => 'root',
+        group => 'root',
+        mode => 0755,
+        require => Package['httpd'],
+      }
+    }
+    'Ubuntu': {
+      package { ['apache2']:
+        ensure => present,
+      }
+
+      group { 'www-data':
+        ensure  => present,
+        require => Package['apache2'],
+      }
+
+      user { 'www-data':
+        ensure => present,
+        require => Group['www-data'],
+      }
+
+      vagrant::group { 'vagrant-www-data':
+        group => 'www-data',
+        require => Group['www-data'],
+      }
+
+      service { 'apache2':
+        enable => true,
+        ensure => running,
+        hasstatus => true,
+        hasrestart => true,
+        require => Package['apache2'],
+      }
+
+    }
   }
 }
-
