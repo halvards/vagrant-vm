9926606fe2c0cc8cfffcaedb086243ba55d8be6d
Halvard Skogsrud <halvard@skogsrud.com>
2013-07-08 10:50:21 +1000

    Updates to the HTTP lab VM: Adding additional tools and removing unnecessary port forwards

--- lab-ubuntu64/manifests/lab-ubuntu64.pp
+++ lab-ubuntu64/manifests/lab-ubuntu64.pp
@@ -2,25 +2,23 @@ stage { 'first': before => Stage['main'], } # 'main' is the default single stage
 class { 'ubuntu::fixes': stage => 'first', } # Ensure basic fixes are installed first
 
 include apache::ab
+include apps::browsermob_proxy
 include apps::chrome
 include apps::phantomjs
 include fonts::microsoft
-include editor::idea-ultimate
-include editor::ideaplugin-nodejs
-include editor::ideaplugin-python
-include editor::ideaplugin-ruby
-#include editor::janus
+include editor::janus
 include editor::sublime
 include editor::vimx
 include editor::webstorm
-include groovy::gradle
 include iptables::disable
 include java::oraclejdk7
 include nodejs::base
-#include python::locust
-include python::virtualenv
+include python::locust
 include ruby::bundler
-include squid::base
+class { 'squid::accelerator':
+  proxy_port       => '80',
+  application_port => '8000',
+}
 include timezone::sydney
 include vcs::git
 include xwindows::hideerrors
