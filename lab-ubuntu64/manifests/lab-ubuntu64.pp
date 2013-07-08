d8519d35e182646d504989eee7734d7cd5bf1697
Halvard Skogsrud <halvard@skogsrud.com>
2013-07-09 00:38:26 +1000

    Updating Lab VM recipe

--- lab-ubuntu64/manifests/lab-ubuntu64.pp
+++ lab-ubuntu64/manifests/lab-ubuntu64.pp
@@ -1,25 +1,19 @@
 stage { 'first': before => Stage['main'], } # 'main' is the default single stage
 class { 'ubuntu::fixes': stage => 'first', } # Ensure basic fixes are installed first
 
-include apache::ab
-include apps::browsermob_proxy
 include apps::chrome
-include apps::phantomjs
-include fonts::microsoft
+include apps::wireshark
 include editor::janus
-include editor::sublime
+include editor::sublime3
 include editor::vimx
 include editor::webstorm
+include fonts::microsoft
 include iptables::disable
 include java::oraclejdk7
 include nodejs::base
-include python::locust
-include ruby::bundler
-class { 'squid::accelerator':
-  proxy_port       => '80',
-  application_port => '8000',
-}
 include timezone::sydney
 include vcs::git
 include xwindows::hideerrors
+
+openssl::certificate { 'localhost': }
 
