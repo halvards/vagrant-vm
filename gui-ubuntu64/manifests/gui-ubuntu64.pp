0acd6b91db5fd7d506c66abb3f2052691264f2b5
Halvard Skogsrud <halvard@skogsrud.com>
2014-10-27 10:05:00 +1100

    Switch from Ubuntu 12.04 (precise) to 14.04 (trusty)
    Also added NodeJS development VM.
    And lots of changes required for new versions of VirtualBox and Vagrant.

--- gui-ubuntu64/manifests/gui-ubuntu64.pp
+++
@@ -1,9 +0,0 @@
-stage { 'first': before => Stage['main'], } # 'main' is the default single stage
-class { 'ubuntu::fixes': stage => 'first', } # Ensure basic fixes are installed first
-
-include apps::evince
-include fonts::microsoft
-include timezone::sydney
-include webbrowser::chrome
-include xwindows::hideerrors
-
